4e7fde31b286aabb4ab14f425e4e5d1d18685322
Karl Reese <reeseka@rose-hulman.edu>
2019-10-31 22:44:35 -0400

    Rudimentary modification to run the example with two cores - fails due to timestep drift

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <mpi.h>
 
 #include <boost/filesystem.hpp>
 #include <boost/numeric/odeint.hpp>
@@ -16,38 +17,83 @@ using namespace boost::numeric::odeint;
 using namespace std;
 
 int main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
+  int mpiRank, mpiSize;
+  MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
+
   TimeLogger &tLogger = TimeLogger::getInstance();
-  tLogger.recordProgramStartTime();
+  if (mpiRank == 0)
+    tLogger.recordProgramStartTime();
 
   po::variables_map vm;
   if (!argparser::parse(argc, argv, vm)) {
+    MPI_Finalize();
     return 0;
   }
 
   const auto &inputFile = vm["input-file"].as<string>();
   const auto &outputFile = vm["output-file"].as<string>();
+  // FIXME: Hack for now until I figure out what C++ would prefer
+  // I want this so I don't have multiple processes writing to the same file.
+  // Ideally, we'll only have one output file, written to by rank 0.
+  stringstream stream;
+  stream << outputFile << mpiRank;
+  const auto &outputFilename = stream.str();
 
-  tLogger.recordLoadConfigStartTime();
+  if (mpiRank == 0)
+    tLogger.recordLoadConfigStartTime();
+
   JsonToProtobufConfigConverter converter;
   Config config = converter.readConfig(const_cast<string &>(inputFile));
+
+  // const auto totalNeurons = config.neurons_size();
+  // const auto totalSynapses = config.neurons_size();
+
+  // // FIXME: Hard-coded like an idiot
+  // int neuronCounts[2] = {1, 1};
+  // int neuronAssignments[2][1] = {{0}, {1}};
+  // int synapseCounts[2] = {1, 1};
+  // int synapseAssignments[2][1] = {{1}, {0}}; //synapse 0 goes from N2 to N1
+
+  //FIXME: Don't hard-code this either, you idiot
+  if (mpiRank == 0)
+  {
+    config.mutable_neurons()->DeleteSubrange(1,1); // rank 0 doesn't need neuron 1
+    config.mutable_synapses()->DeleteSubrange(0,1); // rank 0 doesn't need synapse 9
+  }
+  else
+  {
+    config.mutable_neurons()->DeleteSubrange(0,1);
+    config.mutable_synapses()->DeleteSubrange(1,1);
+  }
+
+  // Now that we've pruned the protobuf config, we'll initialize only the items belonging
+  // to our rank into the ProgramConfig, which will be referenced by the equation.
+  // Notably, we still have vestiges of the deleted items in neurons' "incoming" arrays
+  // and synapses' "source" values. It is paramount to handle these properly in the equation.
+
   config::ProgramConfig &c = config::ProgramConfig::getInstance();
   try {
     c.loadProtobufConfig(config);
   } catch (exception &e) {
     cerr << e.what() << endl;
+    MPI_Finalize();
     return 1;
   }
-  tLogger.recordLoadConfigEndTime();
+  if (mpiRank == 0)
+    tLogger.recordLoadConfigEndTime();
 
   const auto numNeuron = config.neurons_size();
   const int bufferSize = numNeuron + 1;
   const int precision = vm["output-precision"].as<int>();
   const int verbosity = vm["verbose-level"].as<int>();
-  auto buffer = new AsyncBuffer(bufferSize, const_cast<string &>(outputFile), precision, verbosity);
+  auto buffer = new AsyncBuffer(bufferSize, const_cast<string &>(outputFilename), precision, verbosity);
 
   sequential::ode_system_function *equation = factory::equation::getEquation(vm);
 
-  tLogger.recordCalculationStartTime();
+  if (mpiRank == 0)
+    tLogger.recordCalculationStartTime();
   integrate_const(
     make_controlled(
       c.absoluteError,
@@ -68,11 +114,18 @@ int main(int argc, char** argv) {
       buffer->writeData(&(toWrite[0]));
     }
   );
-  tLogger.recordCalculationEndTime();
+  if (mpiRank == 0)
+    tLogger.recordCalculationEndTime();
 
   delete buffer;
-  tLogger.recordProgramEndTime();
-  tLogger.printSummary();
+
+  if (mpiRank == 0)
+  {
+    tLogger.recordProgramEndTime();
+    tLogger.printSummary();
+  }
+
+  MPI_Finalize();
 
   return 0;
 }
--- src/main/math/HodgkinHuxley.cpp
+++ src/main/math/HodgkinHuxley.cpp
@@ -1,4 +1,5 @@
 #include "ODE.h"
+#include <mpi.h>
 
 using namespace config;
 
@@ -11,7 +12,67 @@ ode::hodgkinhuxley::HodgkinHuxleyEquation::HodgkinHuxleyEquation() {
   this->pc = &(ProgramConfig::getInstance());
 }
 
+
+//FIXME - this should iterate over the neurons in this rank and work on each of their synapses
+// This will work because this rank is responsible for every synapse leaving one of its neurons
+void isyns_pt1(double *arrP, double *arrM, double *arrG,
+                                          SynapseConstants *allSynapses, MPI_Win &winXY, MPI_Win &winEXY)
+                                         // ProtobufRepeatedInt32 &ownSynapses, int numOfOwnSynapses) 
+{
+  //FIXME: I don't like copy-pasting this crap all over the place
+  int mpiRank, mpiSize;
+  MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
+
+  //FIXME: THIS IS A HACK!!!!
+  int numOfOwnSynapses = 1;
+  int synapseIndex = 0;
+
+  // TODO Investigate the formula expressed here. It's not the same as in the paper
+  //#pragma omp parallel for reduction(+:result)
+  for (int i = 0; i < numOfOwnSynapses; i++) {
+    //const int synapseIndex = ownSynapses[i];
+    const SynapseConstants &s = allSynapses[i]; //allSynapses[synapseIndex];
+    const double Esyn = s.esyn;
+    const double gbarsyng = s.gbarsyng;
+    const double gbarsyns = s.gbarsyns;
+    const double cGraded = s.cGraded;
+    const double tauDecay = s.tauDecay;
+    const double tauRise = s.tauRise;
+    const double P = arrP[synapseIndex];
+    const double M = arrM[synapseIndex];
+    const double g = arrG[synapseIndex];
+    const double P3 = pow(P, 3);
+    double isyng = gbarsyng * P3 / (cGraded + P3);
+    // TODO Investigate: magic number t0
+    const double t0 = 0;
+    const double tPeak = t0 + (tauDecay * tauRise * log(tauDecay/tauRise)) / (tauDecay - tauRise);
+    // TODO Investigate: why
+    const double fsyns = 1 / (exp(-(tPeak - t0)/tauDecay) + exp(-(tPeak - t0)/tauRise));
+    const double isyns = M * gbarsyns * g * fsyns;
+    
+    double xiyi = isyns + isyng;
+    double esynXiYi = Esyn*xiyi;
+
+    if (mpiRank == 0)
+    { //FIXME - should be MPI_SUM
+      MPI_Accumulate(&xiyi, 1, MPI_DOUBLE, 1, 0, 1, MPI_DOUBLE, MPI_REPLACE, winXY);
+      MPI_Accumulate(&esynXiYi, 1, MPI_DOUBLE, 1, 0, 1, MPI_DOUBLE, MPI_REPLACE, winEXY);
+    }
+    else
+    {
+      MPI_Accumulate(&xiyi, 1, MPI_DOUBLE, 0, 0, 1, MPI_DOUBLE, MPI_REPLACE, winXY);
+      MPI_Accumulate(&esynXiYi, 1, MPI_DOUBLE, 0, 0, 1, MPI_DOUBLE, MPI_REPLACE, winEXY);
+    }
+  }
+}
+
 void ode::hodgkinhuxley::HodgkinHuxleyEquation::calculateNextState(const storage_type &x, storage_type &dxdt, double t) {
+  //FIXME: I don't like copy-pasting this crap all over the place
+  int mpiRank, mpiSize;
+  MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
+
   ProgramConfig &c = *pc;
   double *arrV = c.getVArray(const_cast<storage_type &>(x));
   double *arrMk2 = c.getMk2Array(const_cast<storage_type &>(x));
@@ -57,6 +118,32 @@ void ode::hodgkinhuxley::HodgkinHuxleyEquation::calculateNextState(const storage
 
   const int numOfNeurons = c.numOfNeurons;
   const int numOfSynapses = c.numOfSynapses;
+
+  double* isynsXY;
+  double* isynsEXY;
+  MPI_Win winXY, winEXY;
+  MPI_Alloc_mem(numOfNeurons*sizeof(double), MPI_INFO_NULL, &isynsXY);
+  MPI_Alloc_mem(numOfNeurons*sizeof(double), MPI_INFO_NULL, &isynsEXY);
+  for (int i=0; i<numOfNeurons; i++)
+    isynsXY[i] = isynsEXY[i] = 0;
+  MPI_Win_create(isynsXY, numOfNeurons*sizeof(double), sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &winXY);
+  MPI_Win_create(isynsEXY, numOfNeurons*sizeof(double), sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &winEXY);
+
+  //Open fence
+  MPI_Win_fence(0, winXY);
+  MPI_Win_fence(0, winEXY);
+
+  isyns_pt1(arrP, arrM, arrG, c.getAllSynapseConstants(), winXY, winEXY);
+
+  // Close fence
+  MPI_Win_fence(0, winEXY);
+  MPI_Win_fence(0, winXY);
+  
+
+  MPI_Win_free(&winEXY);
+  MPI_Win_free(&winXY);
+  
+
 #if USE_OPENMP
   #pragma omp parallel for default(shared)
 #endif
@@ -64,6 +151,8 @@ void ode::hodgkinhuxley::HodgkinHuxleyEquation::calculateNextState(const storage
     using namespace std;
     const NeuronConstants &n = c.getNeuronConstantAt(i);
     const double V = arrV[i];
+
+    //std::cout << mpiRank << " has xy=" << isynsXY[i] << " and exy=" << isynsEXY[1] << endl;
 
     // Calculate dVdt
     arrdVdt[i] = -(ina(n.gbarna, arrMna[i], arrHna[i], V, n.ena) +
@@ -76,7 +165,8 @@ void ode::hodgkinhuxley::HodgkinHuxleyEquation::calculateNextState(const storage
                    ikf(n.gbarkf, arrMkf[i], V, n.ek) +
                    ih(n.gbarh, arrMh[i], V, n.eh) +
                    il(n.gbarl, V, n.el) +
-                   isyns(V, arrP, arrM, arrG, c.getAllSynapseConstants(), *(n.incoming), n.incoming->size())
+                   isynsXY[i]*V - isynsEXY[i] /* <-- This is the new Isyns call */
+                   /*isyns(V, arrP, arrM, arrG, c.getAllSynapseConstants(), *(n.incoming), n.incoming->size())*/
     ) / n.capacitance;
 
     // Calculate dMk2dt
@@ -108,13 +198,18 @@ void ode::hodgkinhuxley::HodgkinHuxleyEquation::calculateNextState(const storage
     // Calculate dMhdt
     arrdMhdt[i] = dMhdt(V, arrMh[i]);
   }
+  
+  MPI_Free_mem(isynsEXY);
+  MPI_Free_mem(isynsXY);
+  
 #if USE_OPENMP
   #pragma omp parallel for default(shared)
 #endif
   for (int j = 0; j < numOfSynapses; j++) {
     using namespace std;
     const SynapseConstants &s = c.getSynapseConstantAt(j);
-    const int sourceNeuronIndex = s.source;
+    // FIXME!! - This needs to be translated to our rank's index of neurons.
+    const int sourceNeuronIndex = 0; //s.source;
     const NeuronConstants &n = c.getNeuronConstantAt(sourceNeuronIndex);
     const double V = arrV[sourceNeuronIndex];
 
